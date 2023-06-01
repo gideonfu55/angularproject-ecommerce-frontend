70eea624912090c1ad16935e3634d0c1976de024
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-01 09:03:18 +0800

    refactor cart service addToCart method for-loop

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -23,12 +23,7 @@ export class CartService {
 
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
-      for (const item of this.cartItems) {
-        if (item.id === theCartItem.id) {
-          existingCartItem = item;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(cartItem => cartItem.id === theCartItem.id)!
       // check if we found it
       alreadyExistingInCart = (existingCartItem != undefined);
     }

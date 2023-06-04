201c4d52d5b8b08d6a3824e7717bcea109fb586e
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-04 11:49:06 +0800

    update credit card validation on checkout form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -55,8 +55,8 @@ export class CheckoutComponent implements OnInit {
       creditCard: this.formBuilder.group({
         cardType: new FormControl('', [Validators.required]),
         nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutValidators.notOnlyWhiteSpace]),
-        cardNumber: new FormControl('', [Validators.pattern('[0-9]{16}')]),
-        securityCode: new FormControl('', [Validators.pattern('[0-9]{3}')]),
+        cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth: new FormControl('', [Validators.required]),
         expirationYear: new FormControl('', [Validators.required])
       }),

7bf4e6301fe0b5e75b379ce91f39d7d01b29e199
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-04 16:30:57 +0800

    update instance variables and constructors for all new classes

--- src/app/common/address.ts
+++ src/app/common/address.ts
@@ -1,2 +1,23 @@
 export class Address {
+
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  postCode: string;
+
+  constructor(
+    street: string,
+    city: string,
+    state: string,
+    country: string,
+    postCode: string
+) {
+    this.street = street
+    this.city = city
+    this.state = state
+    this.country = country
+    this.postCode = postCode
+  }
+
 }
--- src/app/common/order.ts
+++ src/app/common/order.ts
@@ -1,2 +1,11 @@
 export class Order {
+
+  totalQuantity: number;
+  totalPrice: number;
+
+  constructor(totalQuantity: number, totalPrice: number) {
+    this.totalQuantity = totalQuantity
+    this.totalPrice = totalPrice
+  }
+
 }

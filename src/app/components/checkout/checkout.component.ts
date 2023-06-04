20938ca072361283b470f47f706df0541a742d11
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-04 09:29:43 +0800

    define custom validator for nowhitespace rule, add validator to form controls, and update HTML for displaying error message

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { CheckoutFormService } from 'src/app/services/checkout-form.service';
+import { CheckoutValidators } from 'src/app/validators/checkout-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -33,8 +34,8 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutValidators.notOnlyWhiteSpace]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutValidators.notOnlyWhiteSpace]),
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
--- src/app/validators/checkout-validators.ts
+++ src/app/validators/checkout-validators.ts
@@ -1,2 +1,16 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
 export class CheckoutValidators {
+
+  // whitespace validation
+  static notOnlyWhiteSpace(control: FormControl): ValidationErrors {
+
+    // check if string only contains whitespace:
+    if ((control.value != null) && (control.value.trim().length === 0)) {
+      return { 'notOnlyWhiteSpace': true };
+    } else {
+      // valid - return empty object
+      return {};
+    }
+  }
 }

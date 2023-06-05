6e2a23b7bd55dc997be6ee53fef48f48a9b4aa08
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-06 01:25:11 +0800

    update paths

--- src/app/common/purchase/purchase.ts
+++ src/app/common/purchase/purchase.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Address } from "./address";
-import { Customer } from "./customer";
-import { Order } from "./order";
-import { OrderItem } from "./order-item";
+import { Address } from "../address/address";
+import { Customer } from "../customer/customer";
+import { Order } from "../order/order";
+import { OrderItem } from "../order-item/order-item";
 
 export class Purchase {
 

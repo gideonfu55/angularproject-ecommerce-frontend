1432b390a44e053c93889907d6c8019da9aa014c
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-06 22:49:39 +0800

    debug auth-interceptor: add space after bear

--- src/app/services/auth-interceptor/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor/auth-interceptor.service.ts
@@ -29,7 +29,7 @@ export class AuthInterceptorService implements HttpInterceptor {
       // clone the request and add new header with access token
       request = request.clone({
         setHeaders: {
-          Authorization: 'Bearer' + accessToken
+          Authorization: 'Bearer ' + accessToken
         }
       });
     }
